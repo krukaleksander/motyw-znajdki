529e81c8ab94fd07ef6f6c9bef54b0dc9ba19132
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-21 19:26:54 +0100

    change slider arrows with js plus basic css

--- js/scaleImg.js
+++ js/scaleImg.js
@@ -1,8 +1,16 @@
 (() => {
+    //part with is changing size of slider image
     var imgWidth = window.innerWidth * 0.8;
     var imgHeightFactor = 0.4;
     var imgGoodHeight = imgWidth * imgHeightFactor;
     var imgContainer = document.querySelector('.slides_container');
     imgContainer.style.height = `${imgGoodHeight}px`;
+
+    // this part change arrows
+
+    var arrowLeft = document.querySelector('.prev');
+    var arrowRight = document.querySelector('.next');
+    arrowLeft.innerText = '<';
+    arrowRight.innerText = '>';
 
 })()
